36b202dbf0d71aa82a1130b7124c3ea82c8ae30e
Harshit <102576901+Harshit-2@users.noreply.github.com>
2023-12-13 00:50:30 +0530

    Add files via upload

---
+++ Xylophone-iOS13/README.md
@@ -0,0 +1,7 @@
+![App Brewery Banner](Documentation/AppBreweryBanner.png)
+
+# Xylophone
+
+## What you will create
+
+Making first musical instrument! Music apps are so popular on the App Store that they even get their own category. So  we’re going to make a colourful XyloPhone app.
---
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
+}
+
